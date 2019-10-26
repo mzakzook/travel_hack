02288d0787705e1f600a285c31de4f98180fa14f
Max Zakzook <mzakzook@me.com>
2019-10-25 19:56:00 -0700

    Welcome page, add category to hack create/update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to root_path
     else
       redirect_to login_path
     end
